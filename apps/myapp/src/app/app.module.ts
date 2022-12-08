804129b0d58ba0a87ea37cfaa7fa3dc2cd957617
George Knap <g.knap@solar-log.com>
2022-12-08 16:56:03 +0100

    add tailwind

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FoolibComponent } from '@repoapp/foolib';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FoolibComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
